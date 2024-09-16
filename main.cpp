d06cab6c197c7f2c269786406711e4a123e3de5b
mmichelotti <m.michelotti@hotmail.it>
2024-09-16 18:14:12 +0200

    clean up main, need some refactoring before proceeding

--- main.cpp
+++ main.cpp
@@ -24,7 +24,6 @@ It doesn't interact with OpenGL directly to render graphics but helps set up the
 It provides a platform-independent API for windowing and input.
 */
 
-const GLint WIDTH = 800, HEIGHT = 600;
 const float toRadians = 3.14159265f / 180.0f; //converting a full circle from 2PI to 360*
 
 
@@ -39,20 +38,6 @@ th Index Buffer recycle those overlapping vertices, so it has a total of 4
 Window mainWindow;
 std::vector<Mesh*> meshes;
 std::vector<Shader> shaders;
-
-
-bool direction = true;
-float triOffset = 0.0f;
-float triMaxOffset = 0.7f;
-float triIncrement = 0.0005f;
-
-float curAngle = 0.0f;
-
-bool sizeDirection = true;
-float curSize = 0.4f;
-float maxSize = 0.8f;
-float minSize = 0.1f;
-
 
 
 //Vertex Shader
@@ -86,8 +71,6 @@ void CreateObjects()
 	meshes.push_back(obj);
 }
 
-
-
 void CreateShaders() 
 {
 	Shader *shader1 = new Shader();
@@ -112,21 +95,6 @@ int main()
 	{
 		glfwPollEvents(); //Get and handle user input events
 
-		if (direction) triOffset += triIncrement;
-		else triOffset -= triIncrement;
-		if (abs(triOffset) >= triMaxOffset)
-		{
-			direction = !direction;
-			sizeDirection = !sizeDirection;
-		}
-
-		curAngle += 0.01f;
-		if (curAngle >= 360) curAngle -= 360;
-
-		if (sizeDirection) curSize += 0.0001f;
-		else curSize -= 0.0001f;
-
-
 		//Clear the whole window
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f); //Set it to white
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //Clear other pixel informations (in this case color and depth buffer)
@@ -136,25 +104,28 @@ int main()
 		uniformModel = shaders[0].GetModelLocation();
 		uniformProjection = shaders[0].GetProjectionLocation();
 
-		
-		glm::mat4 model(1.0f); //create identity matrix 
-			//1 0 0 0 
-			//0 1 0 0
-			//0 0 1 0
-			//0 0 0 1
-		model = glm::translate(model, glm::vec3(0.0f, triOffset*3, -5.0f)); //translate the uniform further so we can see it on the projection matrix
-			//uniform variables are a constant through all the shader, not influenced by each single vertex
+		glm::mat4 model(1.0f); 
+		model = glm::translate(model, glm::vec3(0.0f, 0.0f, -5.0f));
+		model = glm::rotate(model, 0 * toRadians, glm::vec3(1.0f, 1.0f, 1.0f));
+		model = glm::scale(model, glm::vec3(1.0f, 1.0f, 1.0f));
 
-		model = glm::rotate(model, curAngle * toRadians, glm::vec3(0.0f, 1.0f, 0.0f)); //rotate n degrees around the z axis
-			//of course the objects distorts becuase there is no projection matrix applied to the geometry
-			//the default matrix system is the window coordinate system, so as we rotate an object on a non uniform window
-			//the object will deform to match the window coordynate system
-		//model = glm::scale(model, glm::vec3(curSize, curSize, 1.0f));
-			//so the order matters, even when scaling
-			//to make translations absolute we need to apply them first
-			//if we want them relative we need to apply after
-
-
+		//Identity Matrix 
+		/*
+			1 0 0 0 
+			0 1 0 0
+			0 0 1 0
+			0 0 0 1
+		*/
+		//Uniform variables are a constant through all the shader, not influenced by each single vertex
+		/* 
+			Of course the objects distorts becuase there is no projection matrix applied to the geometry
+			the default matrix system is the window coordinate system, so as we rotate an object on a non uniform window
+			the object will deform to match the window coordynate system
+			
+			so the order matters, even when scaling
+			to make translations absolute we need to apply them first
+			if we want them relative we need to apply after
+		*/
 
 		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
 		glUniformMatrix4fv(uniformProjection, 1, GL_FALSE, glm::value_ptr(projection));
